d3cd4138b12760bd32e9adbb595e49a642494486
Milan Margetic <13180642+mkmilan@users.noreply.github.com>
2025-04-17 15:52:43 +0200

    next config add domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["localhost"],
+		domains: ["localhost", "https://travel-server-fdxh.onrender.com"],
 	},
 };
 
 export default nextConfig;
+
+// remotePatterns: [
+//   {
+//       protocol: "http", // Allow http for local development
+//       hostname: "localhost",
+//       port: "5001", // Specify the port your local backend runs on
+//       pathname: "/api/photos/**", // Be specific about the path
+//   },
+//   {
+//       protocol: "https", // Allow https for production
+//       hostname: "travel-server-fdxh.onrender.com", // Your Render backend hostname
+//       port: "", // Default port (443 for https)
+//       pathname: "/api/photos/**", // Be specific about the path
+//   },
+//   // Add any other domains you might load images from (e.g., user profile pics from external services)
+// ],
